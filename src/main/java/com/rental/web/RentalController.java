36284695da1f6a97d6ced45fe53dbbf81bf2d1b7
John <admin@tests-MBP-3.fios-router.home>
2018-09-17 08:46:34 -0400

    Update REST service annotations using the new version.

--- src/main/java/com/rental/web/RentalController.java
+++ src/main/java/com/rental/web/RentalController.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
+import javax.validation.Valid;
 import java.util.*;
 
 @RestController
@@ -19,7 +20,7 @@ public class  RentalController {
     private TenantRepository tenantRepository;
 
     @ApiOperation(value = "Get Rental Properties By ID", notes = "Get Rental Properties By ID")
-    @RequestMapping(method = RequestMethod.GET, path="/rental/{rentalId}", produces = "application/json")
+    @GetMapping(path="/rental/{rentalId}", produces = "application/json")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Success", response = Rental.class),
             @ApiResponse(code = 404, message = "Not Found"),
@@ -31,7 +32,7 @@ public class  RentalController {
     }
 
     @ApiOperation(value = "Get Rental Properties By Street Name", notes = "Get Rental Properties By Street Name")
-    @RequestMapping(method = RequestMethod.GET, path="/rentals/street/{streetName}", produces = "application/json")
+    @GetMapping(path="/rentals/street/{streetName}", produces = "application/json")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Success", response = Rental.class),
             @ApiResponse(code = 404, message = "Not Found"),
@@ -41,7 +42,7 @@ public class  RentalController {
     }
 
     @ApiOperation(value = "Get Rental Properties By City", notes = "Get Rental Properties By City")
-    @RequestMapping(method = RequestMethod.GET, path="/rentals/city/{cityName}", produces = "application/json")
+    @GetMapping(path="/rentals/city/{cityName}", produces = "application/json")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Success", response = Rental.class),
             @ApiResponse(code = 404, message = "Not Found"),
@@ -51,12 +52,12 @@ public class  RentalController {
     }
 
     @ApiOperation(value = "Create Tenant", notes = "Create Tenant")
-    @RequestMapping(method = RequestMethod.POST, path="/tenant/", produces = "application/json")
+    @PostMapping(path="/tenant/", produces = "application/json")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Success", response = Tenant.class),
             @ApiResponse(code = 404, message = "Not Found"),
             @ApiResponse(code = 500, message = "Failure")})
-    public Tenant createTenant(@RequestBody Tenant tenant) {
+    public Tenant createTenant(@Valid @RequestBody Tenant tenant) {
         return tenantRepository.save(tenant);
     }
 }
