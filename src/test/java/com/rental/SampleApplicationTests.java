c68df546fcf301f13c6acf361353e0f0e3c3036d
John <admin@tests-MBP-3.fios-router.home>
2018-10-21 05:57:18 -0400

    Added dummy unit test.

--- src/test/java/com/rental/SampleApplicationTests.java
+++ src/test/java/com/rental/SampleApplicationTests.java
@@ -1,10 +1,12 @@
 package com.rental;
 
+import com.rental.domain.Rental;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.Assert.*;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,6 +18,14 @@ public class SampleApplicationTests {
 		assertTrue(true);
 	}
 
+	@Test
+	public void dummyTest()
+	{
+		Rental rental = new Rental();
+		rental.setCity("NOTTINGHAM");
+
+		assertTrue(rental.getCity() == "NOTTINGHAM");
+	}
 }
 
 
